e048081452339e682e0545bf4f2788551066ec10
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2022-01-26 15:03:20 +0900

    22/01/26
    팰린드롬 분할 - 팰린드롬을 구하는 것 까지는 쉬웠으나 그걸 dp로 구현하는게 어려웠던 문제

--- 1509-팰린드롬_분할.cpp
+++ 1509-팰린드롬_분할.cpp
@@ -13,10 +13,27 @@ int main(){
     cout.tie(0);
 
     cin>>input;
-    
-    N=input.length();
+
+    N=input.size();
     for(int i=0;i<N;i++)    p[i][i]=true;
-    
+    for(int i=0;i<N-1;i++)  if(input[i]==input[i+1]) p[i][i+1]=true;
+    for(int l=2;l<N;l++){
+        for(int i=0;i+l<N;i++){
+            if(input[i]==input[i+l]&&p[i+1][i+l-1])
+                p[i][i+l]=true;
+        }
+    }
+
+    dp[0]=1;
+    for(int i=1;i<N;i++){
+        if(p[0][i]) dp[i]=1;
+        else{
+            dp[i]=dp[i-1]+1;
+            for(int j=1;j<i;j++)    
+                if(p[j][i]) dp[i]=min(dp[i],dp[j-1]+1);
+        }
+    }
+    cout<<dp[N-1]<<'\n';
 
     return 0;
 }
