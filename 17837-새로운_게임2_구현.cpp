ce14a764cff4f7765a60798090125d1ba5d73e0e
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2022-05-31 02:12:09 +0900

    2022/05/31
    새로운 게임2 - 단순 구현 문제 구현문제의 조건을 자세하게 하나씩 살펴보자

---
+++ 17837-새로운_게임2_구현.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+
+using namespace std;
+
+typedef struct Dat{
+    int y,x,m;
+}dat;
+
+vector<dat> wh;
+deque<int> m[15][15];
+
+int color[15][15];
+int my[5]={0,0,0,-1,1};
+int mx[5]={0,1,-1,0,0};
+
+int N,K;
+
+bool mv(){
+    for(int i=0;i<K;i++){
+        int y=wh[i].y;
+        int x=wh[i].x;
+        int me=wh[i].m;
+
+        deque<int> dq;
+        while(m[y][x].back()!=i){
+            dq.push_front(m[y][x].back());
+            m[y][x].pop_back();
+        }
+        m[y][x].pop_back();
+        dq.push_front(i);
+
+        int ny=y+my[me];
+        int nx=x+mx[me];
+
+        if(ny<1||nx<1||ny>N||nx>N||color[ny][nx]==2){//ÆÄ¶û
+            if(me==1)
+                me=2;
+            else if(me==2)
+                me=1;
+            else if(me==3)
+                me=4;
+            else if(me==4)
+                me=3;
+            ny=y+my[me];
+            nx=x+mx[me];
+            wh[i].m=me;
+
+            if(ny<1||nx<1||ny>N||nx>N||color[ny][nx]==2){//ÆÄ¶û -> ÆÄ¶û
+                ny=y;
+                nx=x;
+
+                while(!dq.empty()){
+                    int n=dq.front();
+                    dq.pop_front();
+
+                    m[ny][nx].push_back(n);
+                    wh[n].y=ny;
+                    wh[n].x=nx;
+                }
+            }
+            else if(color[ny][nx]==0){//ÆÄ¶û -> ÇÏ¾ç
+                while(!dq.empty()){
+                    int n=dq.front();
+                    dq.pop_front();
+
+                    m[ny][nx].push_back(n);
+                    wh[n].y=ny;
+                    wh[n].x=nx;
+                }
+            }
+            else if(color[ny][nx]==1){//ÆÄ¶û -> »¡°­
+                while(!dq.empty()){
+                    int n=dq.back();
+                    dq.pop_back();
+
+                    m[ny][nx].push_back(n);
+                    wh[n].y=ny;
+                    wh[n].x=nx;
+                }
+            }
+        }
+        else if(color[ny][nx]==0){//ÇÏ¾ç
+            while(!dq.empty()){
+                int n=dq.front();
+                dq.pop_front();
+
+                m[ny][nx].push_back(n);
+                wh[n].y=ny;
+                wh[n].x=nx;
+            }
+        }
+        else if(color[ny][nx]==1){//»¡°­
+            while(!dq.empty()){
+                int n=dq.back();
+                dq.pop_back();
+
+                m[ny][nx].push_back(n);
+                wh[n].y=ny;
+                wh[n].x=nx;
+            }
+        }
+        
+
+        if(m[ny][nx].size()>=4)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int y,x,me;
+    cin>>N>>K;
+    for(int i=1;i<=N;i++)
+        for(int j=1;j<=N;j++)
+            cin>>color[i][j];
+
+    for(int i=0;i<K;i++){
+        cin>>y>>x>>me; 
+        wh.push_back({y,x,me});
+        m[y][x].push_back(i);
+    }
+
+    int cnt=0;
+    while(++cnt<=1000){
+        if(mv())
+            break;
+    }
+    
+    if(cnt>1000)
+        cout<<-1<<'\n';
+    else
+        cout<<cnt<<'\n';
+
+    return 0;
+}
