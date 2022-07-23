4133e7487e6433c5af50e83a5c4d203b909471c2
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2022-07-23 23:32:55 +0900

    22/07/23
    어른 상어 - 구현은 정말 별곳에서 실수를 다하는 구나 싶다... 왜 좌우로 안움직이나 했더니 좌우에서 x좌표를 안옮기고 y좌표를 옮기는 것을 발견하여 바로해결

--- 19237-어른_상어.cpp
+++ 19237-어른_상어.cpp
@@ -7,15 +7,135 @@ using namespace std;
 int m[4][2] = {
     {-1, 0},
     {1, 0},
-    {-1, 0},
-    {1, 0}};
+    {0, -1},
+    {0, 1}
+};
 
 int sangm[401][4][4];
-int sangdat[401];
+int sangdat[401][3];
 int arr[21][21][2];
 int N, M, K;
+int ret=1001;
 
+bool none_move(int i){
+    int y=sangdat[i][0];
+    int x=sangdat[i][1];
+    int nowm=sangdat[i][2];
 
+    for(int f=0;f<4;f++){
+        int nm=sangm[i][nowm][f];
+        int ny=y+m[nm][0];
+        int nx=x+m[nm][1];
+
+        if(ny<0||nx<0||ny>=N||nx>=N||arr[ny][nx][1]>0)
+            continue;
+    
+        if(arr[ny][nx][1]==-1){
+            sangdat[i][2]=-1;
+            return true;
+        }
+        
+        arr[ny][nx][0]=i;
+        arr[ny][nx][1]=-1;
+        sangdat[i][0]=ny;
+        sangdat[i][1]=nx;
+        sangdat[i][2]=nm;
+        return true;
+    }
+    return false;
+}
+
+void smell_move(int i){
+    int y=sangdat[i][0];
+    int x=sangdat[i][1];
+    int nowm=sangdat[i][2];
+
+    for(int f=0;f<4;f++){
+        int nm=sangm[i][nowm][f];
+        int ny=y+m[nm][0];
+        int nx=x+m[nm][1];
+
+        if(ny<0||nx<0||ny>=N||nx>=N||arr[ny][nx][0]!=i)
+            continue;
+        
+        arr[ny][nx][1]=1001;
+        sangdat[i][0]=ny;
+        sangdat[i][1]=nx;
+        sangdat[i][2]=nm;
+
+        return ;
+    }
+}
+
+void move_fish(){
+    for(int i=1;i<=M;i++){
+        if(sangdat[i][2]==-1)
+            continue;
+
+        if(!none_move(i))
+            smell_move(i);
+    }
+}
+
+void spray_fish(){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(arr[i][j][1]==-1||arr[i][j][1]==1001)
+                arr[i][j][1]=K;
+
+            else if(arr[i][j][1]>0){
+                --arr[i][j][1];
+
+                if(!arr[i][j][1])
+                    arr[i][j][0]=0;
+            }
+        }
+    }
+}
+
+bool chka(){
+    for(int i=2;i<=M;i++){
+        if(sangdat[i][2]!=-1)
+            return false;
+    }
+    return true;
+}
+
+void show(int t){
+    cout<<t<<'\n';
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<arr[i][j][0]<<" ";
+        }
+        cout<<'\t';
+        for(int j=0;j<N;j++){
+            cout<<arr[i][j][0]<<" ";
+        }
+        cout<<'\n';
+    }
+    cout<<'\n';
+    for(int i=0;i<N;i++){
+        cout<<i<<" "<<sangdat[i][0]<<" "<<sangdat[i][1]<<" "<<sangdat[i][2]<<'\n';
+    }
+    cout<<'\n';
+}
+
+void dfs(int t){
+    if(t>1000){
+        ret=t;
+        return;
+    }
+
+    move_fish();
+    spray_fish();
+
+    if(chka()){
+        ret=t;
+        return ;
+    }
+    
+    dfs(t+1);
+}
 
 int main()
 {
@@ -24,38 +144,43 @@ int main()
     cout.tie(0);
 
     cin >> N >> M >> K;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < N; j++)
         {
             cin >> arr[i][j][0];
+            if(!arr[i][j][0])
+                continue;
             arr[i][j][1] = K;
+            sangdat[arr[i][j][0]][0]=i;
+            sangdat[arr[i][j][0]][1]=j;
         }
     }
 
-    for (int i = 1; i <= M; i++)
-        cin >> sangdat[i];
+    for (int i = 1; i <= M; i++){
+        cin >> sangdat[i][2];
+        --sangdat[i][2];
+    }
+        
 
-    vector<vector<int>> tmp(4, vector<int>(2, 0));
     for (int i = 1; i <= M; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            cin >> tmp[0][0] >> tmp[1][0] >> tmp[2][0] >> tmp[3][0];
-            tmp[0][1] = 0;
-            tmp[1][1] = 1;
-            tmp[2][1] = 2;
-            tmp[3][1] = 3;
-
-            sort(tmp.begin(), tmp.end());
-            sangm[i][j][0] = tmp[0][1];
-            sangm[i][j][1] = tmp[1][1];
-            sangm[i][j][2] = tmp[2][1];
-            sangm[i][j][3] = tmp[3][1];
+            cin >> sangm[i][j][0] >> sangm[i][j][1] >> sangm[i][j][2] >> sangm[i][j][3];
+            --sangm[i][j][0];
+            --sangm[i][j][1];
+            --sangm[i][j][2];
+            --sangm[i][j][3];
         }
     }
 
     dfs(1);
+
+    if(ret>1000)
+        cout<<-1<<'\n';
+    else
+        cout<<ret<<'\n';
 
     return 0;
 }
