36d03a0d44b77c0d2ff10aae6119790e651e509c
hangeulkim <ckswls89@naver.com>
2024-10-08 15:50:31 +0900

    08/10/24
    Non-Divisible Subset - implementation, math, greedy
    Repeated String - implementation, math

---
+++ HackerRank/Algorithms/Non-Divisible_Subset.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+int nonDivisibleSubset(int k, vector<int> s)
+{
+    int mod[101] = {
+        0,
+    };
+
+    // calc mod
+    for (int i = 0; i < s.size(); ++i)
+    {
+        ++mod[s[i] % k];
+    }
+
+    int ret = 0;
+
+    // calc possible cases
+    for (int i = 0; i <= k / 2; ++i)
+    {
+        if ((i * 2 == k || i == 0) && mod[i])
+            ret += 1;
+        else
+            ret += max(mod[i], mod[k - i]);
+    }
+
+    return ret;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string s_temp_temp;
+    getline(cin, s_temp_temp);
+
+    vector<string> s_temp = split(rtrim(s_temp_temp));
+
+    vector<int> s(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int s_item = stoi(s_temp[i]);
+
+        s[i] = s_item;
+    }
+
+    int result = nonDivisibleSubset(k, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
+
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
